a66f47b52061ac6926724d8668b354b4320e68be
Omkar Kirpan <okirpan@gmail.com>
2021-03-11 16:28:50 +0530

    app init

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const { app, BrowserWindow } = require("electron");
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "ImageShrink",
+    width: 500,
+    height: 600,
+  });
+
+  mainWindow.loadURL(`https://theunpaiddev.tumblr.com`);
+}
+
+app.on("ready", createMainWindow);
