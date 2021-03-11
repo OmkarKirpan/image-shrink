0ed7947f1c4cebd47ba1b08ed5527a9bf097a49b
Omkar Kirpan <okirpan@gmail.com>
2021-03-11 16:39:00 +0530

    load app from file

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const { app, BrowserWindow } = require("electron");
 
+let mainWindow;
+
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "ImageShrink",
     width: 500,
     height: 600,
+    // icon: './assets/icons/Icon.png',  //<-- Add icon here
   });
 
-  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  //   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile(`./app/index.html`);
 }
 
 app.on("ready", createMainWindow);
