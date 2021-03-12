176c8947d4def907736a710f06fbd7ad9f5f6c03
Omkar Kirpan <okirpan@gmail.com>
2021-03-12 10:25:03 +0530

    Environment and Platform check

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const { app, BrowserWindow } = require("electron");
 
+// Set env
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
 let mainWindow;
 
 function createMainWindow() {
@@ -8,9 +14,22 @@ function createMainWindow() {
     width: 500,
     height: 600,
     // icon: './assets/icons/Icon.png',  //<-- Add icon here
+    resizable: isDev,
   });
 
   mainWindow.loadFile(`./app/index.html`);
 }
 
 app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
